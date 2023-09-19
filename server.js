43c295e3af0d06925d383aad4152d09e2641ff74
Royce119 <madcoder911@gmail.com>
2023-09-19 20:36:47 +0300

    added logging in with google using passport

--- server.js
+++ server.js
@@ -3,12 +3,75 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
+require("dotenv").config();
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
 app.use(helmet());
-app.get("/secret", (req, res) => {
+//Initialize cookie seccion
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["secret key for rotation", "secret key"],
+  })
+);
+//initializing passport middleware
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
+}
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in");
+});
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Secure key is 40");
 });
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 https
   .createServer(
     {
@@ -17,6 +80,6 @@ https
     },
     app
   )
-  .listen(4000, () => {
+  .listen(3000, () => {
     console.log("Server started on port 4000");
   });
